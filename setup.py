00ea2f13cc75d43279f6c055433f03f5a460e914
Caleb Weinreb <calebsw@gmail.com>
2024-01-28 16:42:44 -0700

    remove duplicate name metadata from setup files

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 setuptools.setup(
-    name="snub",
+    name="systems-neuro-browser",
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
 )
